c3ed25023a6b646ff00211563505c9856ea024b4
njaina <98956159+njaina@users.noreply.github.com>
2022-03-31 16:56:02 +0300

    feat:calcul3

--- 3-array-contain.js
+++ 3-array-contain.js
@@ -1,10 +1,14 @@
 function arrayContain(array, number) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === number) {
-      return true;
+  for (let i = 0; i < array.length; i++) { // 2+n+2
+    if (array[i] === number) { // n+1+n
+      return true; //1
     }
   }
-  return false;
+  return false; //1
 }
 
-module.exports = { arrayContain };
+module.exports = { arrayContain }; //1
+/*
+*O(n) = 2+n+2+n+1+n+1+1+1 = 1+n+1+n+1+n+1+1+1 = n (this complexity is linear)
+*/
+
