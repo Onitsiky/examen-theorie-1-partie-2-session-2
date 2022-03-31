5ef047fb8169f0e95fc5404f9a4fb3bf9a0ba5c1
njaina <98956159+njaina@users.noreply.github.com>
2022-03-31 17:18:52 +0300

    feat: calcul 6

--- 6-main.js
+++ 6-main.js
@@ -1,17 +1,20 @@
-const { slice } = require('./1-slice');
-const { merge }= require('./2-merge');
-const { generateUnorderedArray } = require('./4-generate-unordered-array');
+const { slice } = require('./1-slice'); //1
+const { merge }= require('./2-merge'); //1
+const { generateUnorderedArray } = require('./4-generate-unordered-array'); //1
 
 function sort(array) {
-  if (array.length <= 1) {
-    return array;
+  if (array.length <= 1) { //n+1
+    return array; //n
   }
-  const middle = Math.floor(array.length / 2);
-  const left = slice(array, 0, middle);
-  const right = slice(array, middle, array.length);
-  return merge(sort(left), sort(right));
+  const middle = Math.floor(array.length / 2); //1+n+1
+  const left = slice(array, 0, middle); // 1+n+n+1 (n for the slice function and n for the variable array)
+  const right = slice(array, middle, array.length);  // 1 + n+n+1+n
+  return merge(sort(left), sort(right)); // 1
 }
 
-const array = generateUnorderedArray(10);
-console.log(array);
-console.log(sort(array));
+const array = generateUnorderedArray(10); // 2
+console.log(array); //1
+console.log(sort(array)); //1
+/*
+* O(n) = 1 + 1 + 1 + (n+1) * n + 1+n+1 + 1+n+n+1 + 1+n+n+1+n + 1 + 2 +1 + 1 = 1+1+1+n²+n+1+n+1+1+n+n+1+n+1+1+1+1 = n² = T(n²) (quadratic complexity) 
+*/
