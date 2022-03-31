a7bca321c11c1a4d3a3a953943f5fdaa412d0129
njaina <98956159+njaina@users.noreply.github.com>
2022-03-31 17:12:48 +0300

    feat: calcul 4

--- 4-generate-unordered-array.js
+++ 4-generate-unordered-array.js
@@ -1,15 +1,18 @@
-const { arrayContain } = require('./3-array-contain');
+const { arrayContain } = require('./3-array-contain'); //1
 
 function generateUnorderedArray(size) {
-  let array = [];
-  for (let i = 0; i < size; i++) {
-    let number = Math.floor(Math.random() * 100);
-    while (arrayContain(array, number)) {
-      number = Math.floor(Math.random() * 100);
+  let array = []; //1
+  for (let i = 0; i < size; i++) { // 1+1+n+2 
+    let number = Math.floor(Math.random() * 100); // 5
+    while (arrayContain(array, number)) { //1
+      number = Math.floor(Math.random() * 100); //5
     }
-    array.push(number);
+    array.push(number); //1
   }
-  return array;
+  return array; //1
 }
 
-module.exports = { generateUnorderedArray };
+module.exports = { generateUnorderedArray }; //1
+/*
+* O(n) = 1 + 1 + 1+1+n+2 + 5 + 1 + 5 + 1 + 1 + 1 = n = T(n) (linear)
+*/
